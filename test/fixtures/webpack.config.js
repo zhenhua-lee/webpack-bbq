756e66795f8904b92be8821dfc5390bc2113434c
zhengwenbing <zhengwenbing@meituan.com>
2016-04-11 23:37:04 +0800

    fix

--- test/fixtures/webpack.config.js
+++ test/fixtures/webpack.config.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const expose = require('@mtfe/expose');
 
 const config = require('./config');
 const srcpath = require.resolve('./src/');
-const appName = expose(srcpath, `${config.basedir}/src`);
+const appName = expose(srcpath, path.resolve('./src/'));
 
 module.exports = require('../../')(config)({
   entry: { [appName]: srcpath },
