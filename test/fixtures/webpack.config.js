46f0aed5563babdea84b4fbc00d54d0e82c33226
zhengwenbing <zhengwenbing@meituan.com>
2016-04-14 13:41:29 +0800

    add resolve

--- test/fixtures/webpack.config.js
+++ test/fixtures/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = require('./config');
 
 module.exports = require('../../')(config)({
-  entry: require.resolve('./src/'),
+  entry: './src/',
 }, {
-  entry: require.resolve('./src/server'),
+  entry: './src/server',
 });
